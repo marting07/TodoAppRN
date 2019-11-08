d8e56b2137270877e384a3ef5469c4682d324088
Martin Ruiz <jmruiz@sciodev.com>
2019-11-08 14:32:10 -0600

    Added header, input and button components. Added logic in the app component which is the parent component.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
+import React from 'react'
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from './app/App';
 
-AppRegistry.registerComponent(appName, () => App);
+const TodoApp = () => <App />
+
+AppRegistry.registerComponent('TodoApp', () => TodoApp);  // Entry point
